1813f1bed67008bc8093d2a0ca8c1125d094cd49
eidolonz <the_heaven_knights@hotmail.com>
2017-07-11 20:49:39 +0700

    test AlertDialog

--- app/src/main/java/com/jumrus/peerapon/storagemanagement/MainActivity.java
+++ app/src/main/java/com/jumrus/peerapon/storagemanagement/MainActivity.java
@@ -1,7 +1,14 @@
 package com.jumrus.peerapon.storagemanagement;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTest = (Button) findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                final AlertDialog alertDialog = new AlertDialog.Builder(view.getContext()).create();
+                alertDialog.setTitle("Title");
+                alertDialog.setMessage("Message");
+
+                alertDialog.setButton(1,"Continue..", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alertDialog.cancel();
+                    }
+                });
+
+                alertDialog.show();
+            }
+        });
     }
+
+
+
+
 }
